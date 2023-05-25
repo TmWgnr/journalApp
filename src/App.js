ac716ea66ff0e25d11a6aca64fa208ad045d7bdf
Tim Wagner <tim.w@gner-mail.de>
2023-05-25 08:19:04 +0200

    journalapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import EntriesSection from "./components/EntriesSection";
+import EntryForm from "./components/EntryForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import "./App.css";
+
+const initialEntries = [
+  {
+    id: 900,
+    date: "May 20, 2023",
+    motto: "motto",
+    notes: "notes",
+  },
+  {
+    id: 800,
+    date: "May 19, 2023",
+    motto: "motto",
+    notes: "notes",
+  },
+];
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+  const [filter, setFilter] = useState("all");
+
+  function handleToggleFavorite(id) {
+    setEntries(
+      entries.map((entry) =>
+        entry.id === id ? { ...entry, isFavorite: !entry.isFavorite } : entry
+      )
+    );
+  }
+
+  function handleShowFavoriteEntries() {
+    setFilter("favorites");
+  }
+
+  function handleShowAllEntries() {
+    setFilter("all");
+  }
+
+  const favoriteEntries = entries.filter((entry) => entry.isFavorite);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <main className="app__main">
+        <EntryForm />
+        <EntriesSection
+          entries={filter === "favorites" ? favoriteEntries : entries}
+          filter={filter}
+          allEntriesCount={entries.length}
+          favoriteEntriesCount={favoriteEntries.length}
+          onToggleFavorite={handleToggleFavorite}
+          onShowAllEntries={handleShowAllEntries}
+          onShowFavoriteEntries={handleShowFavoriteEntries}
+        />
+      </main>
+      <Footer />
     </div>
   );
 }
